a1aedc04e11504837b466a9eb4a4d49c96ddbd02
Rex Chan <ccm.rex17@outlook.com>
2026-02-07 02:33:39 +0800

    Adjust skillset

--- app/components/Languages.jsx
+++ app/components/Languages.jsx
@@ -6,16 +6,16 @@ const Languages = () => {
     { id: 2, logos: ["https://cdn.shopify.com/s/files/1/0626/2253/1645/files/hydrogen-logo.png", "https://cdn.shopify.com/s/files/1/0626/2253/1645/files/javascript-logo.png"] },
     { id: 3, logos:  ["https://cdn.shopify.com/s/files/1/0626/2253/1645/files/css-logo.png", "https://cdn.shopify.com/s/files/1/0626/2253/1645/files/html-logo.png"] },
     { id: 4, logos: ["https://cdn.shopify.com/s/files/1/0626/2253/1645/files/bootstrap-logo.png", "https://cdn.shopify.com/s/files/1/0626/2253/1645/files/slick-logo.png"] },
-    { id: 5, logos: ["https://cdn.shopify.com/s/files/1/0626/2253/1645/files/three-js-logo.png", "https://cdn.shopify.com/s/files/1/0626/2253/1645/files/liquid-logo.png"] },
+    { id: 5, logos: ["https://cdn.shopify.com/s/files/1/0626/2253/1645/files/jquery-logo.png", "https://cdn.shopify.com/s/files/1/0626/2253/1645/files/three-js-logo.png"] },
     { id: 6, logos: ["https://cdn.shopify.com/s/files/1/0626/2253/1645/files/php8-logo.png", "https://cdn.shopify.com/s/files/1/0626/2253/1645/files/sql-logo.png?v=1770401524"] },
-    { id: 7, logos: ["https://cdn.shopify.com/s/files/1/0626/2253/1645/files/c-sharp-logo.png", "https://cdn.shopify.com/s/files/1/0626/2253/1645/files/java-logo.png"] },
+    { id: 7, logos: ["https://cdn.shopify.com/s/files/1/0626/2253/1645/files/liquid-logo.png", "https://cdn.shopify.com/s/files/1/0626/2253/1645/files/c-sharp-logo.png"] },
   ];
 
   return (
     <div id="languages-container" className="article-section">
         <section className="section-header">
             <h3 className="section-heading">Skills</h3>
-            <p>The following presents my knowledge tree in the industry.</p>
+            <p>The following presents my skillset in the industry.</p>
         </section>
         <div className="language-list-wrapper">
             <ul class="language-list">
